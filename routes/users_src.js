b7b6e57c65a1383554cac9bd5f2e6d8127522a25
JerinJ <jerinj@cybage.com>
2013-08-29 14:52:05 +0530

    Refactoring users call.

--- routes/users_src.js
+++ routes/users_src.js
@@ -5,20 +5,14 @@
 
     exports.createUser = function(req, res) {
         var requestBody = req.body;
-        tbl_users.create({
-            empid: parseInt(requestBody.empid, 10),
-            email: requestBody.email,
-            firstname: requestBody.firstname,
-            lastname: requestBody.lastname,
-            accesstype: parseInt(requestBody.accesstype, 10),
-            password: requestBody.password
-        }).on("success", function(user) {
+        tbl_users.create(requestBody).on("success", function(user) {
             res.format({
                 json: function() {
-                    // console.log(typeof user);
                     res.send(user);
                 }
             });
+        }).on("error", function(error) {
+            console.log(error);
         });
     };
 
@@ -37,6 +31,8 @@
                     res.send(user);
                 }
             });
+        }).on("error", function(error) {
+            console.log(error);
         });
     };
 
@@ -58,23 +54,21 @@
                     res.send(user);
                 }
             });
+        }).on("error", function(error) {
+            console.log(error);
         });
     };
 
     exports.putUsersById = function(req, res) {
         var requestBody = req.body;
-        tbl_users.update({
-            email: requestBody.email,
-            firstname: requestBody.firstname,
-            lastname: requestBody.lastname,
-            accesstype: parseInt(requestBody.accesstype, 10),
-            password: requestBody.password
-        }, {
+        tbl_users.update(requestBody, {
             empid: parseInt(req.params.id, 10)
         }).success(function() {
             tbl_users.findAll().success(function(user) {
                 res.send(user);
             });
+        }).on("error", function(error) {
+            console.log(error);
         });
     };
 
@@ -87,6 +81,8 @@
                     res.send(user);
                 }
             });
+        }).on("error", function(error) {
+            console.log(error);
         });
     };
 }(exports));
