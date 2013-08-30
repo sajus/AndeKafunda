30bf11d4609109f3798af6b851b313068eef45dc
ashwinh <ashwinh@cybage.com>
2013-08-30 12:56:38 +0530

    dbRollOut script added; Routes clean up;
    Signed-off-by: ashwinh <ashwinh@cybage.com>

--- db_release/BhejaFry-RollBack.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE interviewer;
-DROP TABLE recruiter;
-DROP TABLE users;
-DROP TABLE status;
-DROP TABLE roundOfInterview;
-DROP TABLE modeOfInterview;
-DROP TABLE interviewResponse
---
+++ models/dbRollOut.js
@@ -0,0 +1,44 @@
+var sequelize = require('../dbconfig').sequelize;
+
+/**
+  * Database Layer
+***/
+var tbl_users         = sequelize.import(__dirname + '/create/tbl_users')
+  , tbl_greetings     = sequelize.import(__dirname + '/create/tbl_greetings')
+  , tbl_response      = sequelize.import(__dirname + '/create/tbl_response');
+
+tbl_users.hasMany(tbl_response, { onDelete: 'cascade', onUpdate: 'cascade' });
+tbl_greetings.hasMany(tbl_response, { onDelete: 'cascade', onUpdate: 'cascade' });
+tbl_users.hasMany(tbl_greetings, { onDelete: 'cascade', onUpdate: 'cascade' });
+
+
+sequelize.sync().on('success', function() {
+    tbl_users
+    .bulkCreate([
+        {empid:7601, email:'sajus@cybage.com',  firstname: 'Saju', lastname: 'Sasidharan', password: 'sajus', accesstype: 1 },
+        {empid:10789, email:'vinayakpat@cybage.com',  firstname: 'Vinayak', lastname: 'Patil', password: 'vinayakpat', accesstype: 0 }
+    ])
+    .on('success', function(options) {
+        console.log("Users Table Created");
+    })
+});
+
+sequelize.sync().on('success', function() {
+    tbl_greetings
+    .create(
+        {empid: 7601, url: '/uploads/google.png'}
+    )
+    .on('success', function(options) {
+        console.log("Greeting Table Created");
+    })
+});
+
+sequelize.sync().on('success', function() {
+    tbl_response
+    .create(
+        {empid: 7601, greetingid: 1, hasresponse: true}
+    )
+    .on('success', function(options) {
+        console.log("Response Table Created");
+    })
+});
