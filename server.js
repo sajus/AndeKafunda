526d57d0cac6e3ff7f687d629225b2f26cad36f4
JerinJ <jerinj@cybage.com>
2013-08-30 13:06:02 +0530

    adding some more calls for greeting and response
    Signed-off-by: JerinJ <jerinj@cybage.com>

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
         app = express(),
         authorization = require('./routes/authorization_src'),
         users = require('./routes/users_src'),
+        greetings = require('./routes/greetings_src'),
+        responses = require('./routes/responses_src'),
         config = require("./dbresources"),
         db = config.database;
 
@@ -16,18 +18,33 @@
         app.set(express.router);
         app.use(express.static(__dirname + '/src'));
     });
-    /*Authorization call*/
+    /*Authorization Call*/
     app.post('/checkAuthorization', authorization.postAuthorization);
 
-    /*Users call*/
+    /*Users Calls*/
     app.post('/getUsers', users.createUser);
     app.get('/getUsers', users.getUsersList);
     app.get('/getUsers/:id', users.getUsersById);
     app.put('/getUsers/:id', users.putUsersById);
     app.del('/getUsers/:id', users.delUserById);
 
-    /*Admin call*/
+    /*Admin Call*/
     app.get('/getAdmins', users.getAdmins);
+
+    /*Greeting Calls*/
+    app.post('/getGreetings', greetings.createGreeting);
+    app.get('/getGreetings', greetings.getGreetingsList);
+    app.get('/getGreetings/:id', greetings.getGreetingsById);
+    app.put('/getGreetings/:id', greetings.putGreetingsById);
+    app.del('/getGreetings/:id', greetings.delGreetingsById);
+
+    /*Respose Calls*/
+    app.post('/getResponses', responses.createResponse);
+    app.get('/getResponses', responses.getResponsesList);
+    app.get('/getResponses/:empid', responses.getResponsesByEmpId);
+    app.get('/getResponses/greetings/:gid', responses.getResponsesByGreetId);
+    app.get('/getResponses/greetingsCount/:gid', responses.getResponsesByGreetIdCount);
+    app.get('/greetingsCountAll', responses.getResponsesByGreetIdCountAll);
 
     http.createServer(app).listen(app.get('port'), function() {
         console.log("\n\n\tNode (Express) server listening on port " + app.get('port'));
