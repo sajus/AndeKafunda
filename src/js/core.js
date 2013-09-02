b6dc87d7c0bd3c811e24bfe7c1f95491e3f2104d
Vinayak Patil <vinayakpat@cybage.com>
2013-09-02 11:11:19 +0530

    cleans up code for jslint and adds event unbinding.

--- src/js/core.js
+++ src/js/core.js
@@ -1,93 +1,50 @@
-define(['backbone', 
-    'events', 
-    'globals',
-    'views/login/loginView',
-    'models/login/loginModel',
-    'views/defaultAction/accessForbiden',
-    'models/interview/interviewerModel',
-    'models/interview/modeModel',
-    'models/interview/recruiterModel',
-    'models/interview/roundsModel',
-    'models/interview/statusModel',
-    'underscore',
-    'jqueryCookie'
-    ],
-    function(Backbone,
-        Events,
-        Globals,
-        LoginView,
-        LoginModel,
-        AccessForbidenView,
-        InterviewerModel,
-        ModeModel,
-        RecruiterModel,
-        RoundsModel,
-        StatusModel
-        ){
+define(function(require) {
+    "use strict";
+    var Backbone = require('backbone'),
+        Events = require('events'),
+        Globals = require('globals'),
+        LoginView = require('views/login/loginView'),
+        LoginModel = require('models/login/loginModel'),
+        AccessForbidenView = require('views/defaultAction/accessForbiden'),
+        _ = require('underscore'),
+        globals = {},
+        views = {},
+        user = [],
+        create = null;
 
-    _.extend(Backbone.Model , {
-        gateWayUrl:"http://"+document.domain+":"+Globals.gateWayPort
+    require('jqueryCookie');
+    _.extend(Backbone.Model, {
+        gateWayUrl: "http://" + document.domain + ":" + Globals.gateWayPort
     });
 
-    var globals = {};
-  
-    var interviewerModel = new InterviewerModel();
-    var modeModel = new ModeModel();
-    var recruiterModel = new RecruiterModel();
-    var roundsModel = new RoundsModel();
-    var statusModel = new StatusModel();
-
-    interviewerModel.fetch({
-        success:function(){
-            globals.interviewer_list =  interviewerModel.toJSON();
-        }
-    });
-    modeModel.fetch({
-        success:function(){
-            globals.interviewmode_list =  modeModel.toJSON();
-        }
-    });
-    recruiterModel.fetch({
-        success:function(){
-            globals.recruiter_list =  recruiterModel.toJSON();
-        }
-    });
-    roundsModel.fetch({
-        success:function(){
-            globals.interviewrounds_list =  roundsModel.toJSON();
-        }
-    });
-    statusModel.fetch({
-        success:function(){
-            globals.interviewstatus_list =  statusModel.toJSON();
-        }
-    });
-     
-    var views = {},
-        user = ['UserAssesmentPage', 'DashboardPage', 'NewSurvey', 'SurveyDetailed', 'SurveyUserDetailed', 'ListSurvey', 'userPage'];
-
-    var create = function (context, name, View, options) {
-        /*
-            View clean up isn't actually implemented yet,
-            but will simply call .clean, .remove and .unbind
-        */
-        if(typeof views[name] !== 'undefined') {
+    create = function(context, name, View, options) {
+        if (views[name] !== 'undefined') {
+            views[name].remove();
             views[name].undelegateEvents();
-            if(typeof views[name].clean === 'function') {
-                views[name].clean();
+            if (typeof views[name].onClose === 'function') {
+                views[name].onClose();
             }
         }
-        var skipAuthCheck=false;
-        if(options!==undefined){
-            if(options.skipAuthCheck){
-                skipAuthCheck=true;
+
+        var skipAuthCheck = false,
+            accesslevel = $.cookie('accesslevel'),
+            loginModel = null,
+            view = null;
+
+        if (options !== undefined) {
+            if (options.skipAuthCheck) {
+                skipAuthCheck = true;
             }
         }
-        var accesslevel = $.cookie('accesslevel');
-        if(!$.cookie('isAuthenticated') && !skipAuthCheck){
-            var loginModel=new LoginModel(),
-            view = new LoginView({model:loginModel,authorizationFailed:!skipAuthCheck,targetView:View,targetOptions:options});
-        } else if((accesslevel === "admin" && name === "userPage") || (accesslevel === "user" && _.contains(user, name))){
+        if (!$.cookie('isAuthenticated') && !skipAuthCheck) {
+            loginModel = new LoginModel();
+            view = new LoginView({
+                model: loginModel,
+                authorizationFailed: !skipAuthCheck,
+                targetView: View,
+                targetOptions: options
+            });
+        } else if ((accesslevel === "admin" && name === "userPage") || (accesslevel === "user" && _.contains(user, name))) {
             view = new AccessForbidenView();
             Events.trigger("view:navigate", {
                 path: "accessForbiden",
@@ -96,25 +53,17 @@ define(['backbone',
                 }
             });
         } else {
-            var view = new View(options);
+            view = new View(options);
         }
 
         views[name] = view;
 
-        if(typeof context.children === 'undefined'){
-            context.children = {};
-            context.children[name] = view;
-        } else {
-            context.children[name] = view;
-        }
-
-        Events.trigger('viewCreated');
         return view;
     };
 
     return {
         create: create,
-        globals:globals
+        globals: globals
     };
 
 
