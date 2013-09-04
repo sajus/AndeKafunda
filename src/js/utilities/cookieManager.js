c0dabd28bf8809ecd67a4352d549c33c1cf2e864
Vinayak Patil <vinayakpat@cybage.com>
2013-09-04 16:42:16 +0530

    updates cookieManager

--- src/js/utilities/cookieManager.js
+++ src/js/utilities/cookieManager.js
@@ -3,19 +3,25 @@ define(function(require) {
     require('jqueryCookie');
     var _setSessionCookie = function(data) {
         $.cookie('isAuthenticated', true);
+        $.cookie('isAdmin', data.accesstype);
         $.cookie('email', data.email);
     },
         _destroySessionCookie = function() {
             $.removeCookie('isAuthenticated');
+            $.removeCookie('isAdmin');
             $.removeCookie('email');
         },
         _isAuthenticated = function() {
             return $.cookie('isAuthenticated');
+        },
+        _isAdmin = function() {
+            return !!(parseInt($.cookie('isAdmin'), 10));
         };
 
     return {
         setSessionCookie: _setSessionCookie,
         destroySessionCookie: _destroySessionCookie,
-        isAuthenticated: _isAuthenticated
+        isAuthenticated: _isAuthenticated,
+        isAdmin: _isAdmin
     };
 });
