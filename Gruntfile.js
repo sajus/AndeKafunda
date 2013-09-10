3319eae57bacdb3fc578c00baee8515c5fb323a4
Vinayak Patil <vinayakpat@cybage.com>
2013-09-10 18:58:25 +0530

    Adds image-compression and js minification tasks to grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,54 @@ module.exports = function(grunt) {
                     google: true
                 }
             }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dynamic_mappings: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.js'],
+                    dest: 'build',
+                    ext: '.js'
+                }, {
+                    expand: true,
+                    cwd: 'models/',
+                    src: ['**/*.js'],
+                    dest: 'build/models',
+                    ext: '.js'
+                }, {
+                    expand: true,
+                    cwd: 'routes/',
+                    src: ['**/*.js'],
+                    dest: 'build/routes',
+                    ext: '.js'
+                }]
+            }
+        },
+        img: {
+
+            task1: {
+                src: 'uploads',
+                dest: 'build/images/uploads'
+            },
+
+            task2: {
+                src: 'src/imgs',
+                dest: 'build/images/src'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
-
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-img');
     // Default task(s).
     grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('minify', ['uglify']);
+    grunt.registerTask('compressImages', ['img']);
 
 };
